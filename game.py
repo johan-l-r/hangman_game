c947895d7093beeec61742f2de02ac7a67447901
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2026-02-05 12:44:56 -0500

    feat(game): add confirmation popup
    when tue user types a letter a popup is displayed to confirm the letter

--- game.py
+++ game.py
@@ -29,12 +29,33 @@ class Game:
     self.stdscr.clear()
 
     word = word_generator.get_random_word()
-    self.show_board(word, len(word))
-    self.stdscr.getch()
+    ATTEMPTS = 6
+
+    self.show_board(word)
+
+    while ATTEMPTS > 0:
+      self.show_popup()
+      ATTEMPTS -= 1
 
     self.stdscr.refresh()
 
-  def show_board(self, word,  length):
+  def show_popup(self):
+    letter = self.stdscr.getch()
+    message = f"you typed the letter {chr(letter)}"
+
+    POPUP_WIDTH = len(message) + 6
+    POPUP_HEIGHT = 5
+
+    message_x_pos = (POPUP_WIDTH - len(message)) // 2 
+    message_y_pos = POPUP_HEIGHT // 2
+
+    confirmation_popup = curses.newwin(POPUP_HEIGHT, POPUP_WIDTH, 4, 4)
+
+    confirmation_popup.box()
+    confirmation_popup.addstr(message_y_pos, message_x_pos, message)
+    confirmation_popup.getch()
+
+  def show_board(self, word):
     for char in word:
       if char == " ":
         self.stdscr.addstr(" ")
